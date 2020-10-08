03f7bc78f2d517be49d8dec2c0caa9fc827f2963
Shohei Imamura <s3imamura@gmail.com>
2020-10-09 08:37:17 +0900

    第１章 計算とフォーマットにフェーズを分割 フェーズ感で受け渡す中間データ構造を追加 amountFor

--- main.js
+++ main.js
@@ -10,18 +10,42 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(aPerformance);
+    result.amount = amountFor(aPerformance);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unkwon type: ${aPerformance.play.type}`);
+    }
+    return result;
+  }
 }
 
 function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${
       perf.audience
     } seats)\n`;
   }
@@ -33,7 +57,7 @@ function renderPlainText(data, invoice, plays) {
     let result = 0;
     for (let perf of data.performances) {
       // 注文の内訳を出力
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -62,28 +86,6 @@ function renderPlainText(data, invoice, plays) {
     return result;
   }
 
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unkwon type: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
 }
 
 const result = statement(invoice, plays);
