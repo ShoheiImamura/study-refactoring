0b00c9c7c55e542b2cce25ab5ef4c24a4e7bbe4e
Shohei Imamura <s3imamura@gmail.com>
2020-10-08 21:29:36 +0900

    第1章 statement 関数の分割 変数名変更

--- main.js
+++ main.js
@@ -26,31 +26,31 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits \n`;
     return result;
   }
-}
 
-// 関数の分割
-function amountFor(perf, play) {
-  let thisAmount = 0;
+  // 関数の分割
+  function amountFor(perf, play) {
+    let result = 0;
 
-  switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-      throw new Error(`unkwon type: ${play.type}`);
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (perf.audience > 30) {
+          result += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (perf.audience > 20) {
+          result += 10000 + 500 * (perf.audience - 20);
+        }
+        result += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`unkwon type: ${play.type}`);
+    }
+
+    return result;
   }
-
-  return thisAmount;
 }
 
 const result = statement(invoice, plays);
