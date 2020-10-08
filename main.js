2b45e39654e8a017a34e8d9335184a36ab9f74fa
Shohei Imamura <s3imamura@gmail.com>
2020-10-09 08:31:07 +0900

    第１章 計算とフォーマットにフェーズを分割 フェーズ感で受け渡す中間データ構造を追加 playFor

--- main.js
+++ main.js
@@ -4,14 +4,24 @@ const plays = require("./plays.json");
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, invoice, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(aPerformance);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
 
 function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
   }
@@ -47,19 +57,15 @@ function renderPlainText(data, invoice, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" == playFor(aPerformance).type)
+    if ("comedy" == aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
   }
 
   function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -74,7 +80,7 @@ function renderPlainText(data, invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unkwon type: ${playFor(aPerformance).type}`);
+        throw new Error(`unkwon type: ${aPerformance.play.type}`);
     }
     return result;
   }
