1b00b7c64419507315f3359343d869bd08c1bf67
Shohei Imamura <s3imamura@gmail.com>
2020-10-10 13:26:43 +0900

    関数宣言の変更

--- createStatementData.js
+++ createStatementData.js
@@ -7,8 +7,12 @@ export default function createStatementData(invoice, plays) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredit = volumeCreditsFor(result);
     return result;
@@ -57,7 +61,8 @@ export default function createStatementData(invoice, plays) {
 }
 
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
   }
 }
